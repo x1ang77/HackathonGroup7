b10d0d029b05f3e77c4d9d74af18043d6df16e0f
Xiang Ze <xchuay@gmail.com>
2023-02-13 16:46:37 +0800

    merge changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
         "./src/**/*.{html,js}",
@@ -16,19 +15,6 @@ module.exports = {
                 "9xl": "900px",
             },
         },
-=======
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{html,js}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-  ],
-  theme: {
-    colors: {
-      blue: "#163E61",
-      white: "#FFFFFF",
-      lightblue: "#F0F8FF",
-      gray: "#8E8E8E",
->>>>>>> 7418848acbf99b48dcd39c373a7628cea9b424a1
     },
     plugins: [require("tw-elements/dist/plugin")],
 };
