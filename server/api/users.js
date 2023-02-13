dc51695659525798c4b66784e7ef4483f4a6c006
Xiang Ze <xchuay@gmail.com>
2023-02-13 17:24:45 +0800

    merge

--- server/api/users.js
+++ server/api/users.js
@@ -95,14 +95,14 @@ router.get("/:id", async (req, res) => {
     }
 });
 
-router.delete("/:id", auth, async (req, res) => {
-    try {
-    } catch (e) {
-        return res.status(400).json({
-            e,
-            message: "Cannot get user",
-        });
-    }
-});
+// router.delete("/:id", auth, async (req, res) => {
+//     try {
+//     } catch (e) {
+//         return res.status(400).json({
+//             e,
+//             message: "Cannot get user",
+//         });
+//     }
+// });
 
 module.exports = router;
