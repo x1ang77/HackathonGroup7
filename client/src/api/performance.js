a6b541ae826d8fc8a71b29b6c330034e4226a477
joelBlixify <joel@blixify.co>
2023-02-14 19:52:21 +0800

    add get all performance scores in client

--- client/src/api/performance.js
+++ client/src/api/performance.js
@@ -1,40 +1,59 @@
 export const ratePerformance = async (ratings, id) => {
-  try {
-    const res = await fetch(
-      `${process.env.REACT_APP_API_SERVER}/performance/${id}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-auth-token": localStorage.getItem("token"),
-        },
-        body: JSON.stringify(ratings),
-      }
-    );
-    const data = await res.json();
-    console.log(data);
-    if (res.ok) return data;
-    if (!res.ok) throw new Error(data.message);
-  } catch (e) {
-    throw e.message;
-  }
+    try {
+        const res = await fetch(
+            `${process.env.REACT_APP_API_SERVER}/performance/${id}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-auth-token": localStorage.getItem("token"),
+                },
+                body: JSON.stringify(ratings),
+            }
+        );
+        const data = await res.json();
+        console.log(data);
+        if (res.ok) return data;
+        if (!res.ok) throw new Error(data.message);
+    } catch (e) {
+        throw e.message;
+    }
 };
 
 export const performanceScore = async (id) => {
-  try {
-    const res = await fetch(
-      `${process.env.REACT_APP_API_SERVER}/performance/${id}`,
-      {
-        method: "GET",
-        headers: {
-          "x-auth-token": localStorage.getItem("token"),
-        },
-      }
-    );
-    const data = await res.json();
-    if (res.ok) return data;
-    if (!res.ok) throw new Error(data.message);
-  } catch (e) {
-    throw e.message;
-  }
+    try {
+        const res = await fetch(
+            `${process.env.REACT_APP_API_SERVER}/performance/${id}`,
+            {
+                method: "GET",
+                headers: {
+                    "x-auth-token": localStorage.getItem("token"),
+                },
+            }
+        );
+        const data = await res.json();
+        if (res.ok) return data;
+        if (!res.ok) throw new Error(data.message);
+    } catch (e) {
+        throw e.message;
+    }
+};
+
+export const getAllPerformanceScores = async () => {
+    try {
+        const res = await fetch(
+            `${process.env.REACT_APP_API_SERVER}/performance`,
+            {
+                method: "GET",
+                headers: {
+                    "x-auth-token": localStorage.getItem("token"),
+                },
+            }
+        );
+        const data = await res.json();
+        if (res.ok) return data;
+        if (!res.ok) throw new Error(data.message);
+    } catch (e) {
+        throw e.message;
+    }
 };
