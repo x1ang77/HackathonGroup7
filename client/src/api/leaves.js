b6a893e543dc464641b6c050e2b1412d8055649d
Caaron <arn_0000@hotmail.com>
2023-02-14 17:06:14 +0800

    2.7commit

--- client/src/api/leaves.js
+++ client/src/api/leaves.js
@@ -1,80 +1,80 @@
 export const getLeaves = async (leaves) => {
-    try {
-        const res = await fetch(`${process.env.REACT_APP_API_SERVER}/leaves`, {
-            method: "GET",
-            headers: {
-                "x-auth-token": localStorage.getItem("token"),
-            },
-        });
-        const data = await res.json();
-        if (res.ok) return data;
-        if (!res.ok) throw new Error(data.message);
-    } catch (e) {
-        throw e.message;
-    }
+  try {
+    const res = await fetch(`${process.env.REACT_APP_API_SERVER}/leaves`, {
+      method: "GET",
+      headers: {
+        "x-auth-token": localStorage.getItem("token"),
+      },
+    });
+    const data = await res.json();
+    if (res.ok) return data;
+    if (!res.ok) throw new Error(data.message);
+  } catch (e) {
+    throw e.message;
+  }
 };
 
 export const getLeaveById = async (id) => {
-    try {
-        const res = await fetch(
-            `${process.env.REACT_APP_API_SERVER}/leaves/${id}`,
-            {
-                method: "GET",
-                headers: { "x-auth-token": localStorage.getItem("token") },
-            }
-        );
-        const data = await res.json();
-        if (res.ok) return data;
-        if (!res.ok) throw new Error(data.message);
-    } catch (e) {
-        throw e.message;
-    }
+  try {
+    const res = await fetch(
+      `${process.env.REACT_APP_API_SERVER}/leaves/${id}`,
+      {
+        method: "GET",
+        headers: { "x-auth-token": localStorage.getItem("token") },
+      }
+    );
+    const data = await res.json();
+    if (res.ok) return data;
+    if (!res.ok) throw new Error(data.message);
+  } catch (e) {
+    throw e.message;
+  }
 };
 
 export const applyLeave = async (leave, image) => {
-    try {
-        const formData = new FormData();
-        formData.append("typeOfLeave", leave.typeOfLeave);
-        formData.append("details", leave.details);
-        formData.append("date", leave.date);
-        if (image) {
-            formData.append("image", image);
-        }
-
-        console.log(formData);
-        const res = await fetch(`${process.env.REACT_APP_API_SERVER}/leaves`, {
-            method: "POST",
-            headers: {
-                // "Content-Type": "application/json",
-                "x-auth-token": localStorage.getItem("token"),
-            },
-            body: formData,
-        });
-        const data = await res.json();
-        if (res.ok) return data;
-        if (!res.ok) throw new Error(data.message);
-    } catch (e) {
-        throw e.message;
+  try {
+    const formData = new FormData();
+    formData.append("typeOfLeave", leave.typeOfLeave);
+    formData.append("details", leave.details);
+    formData.append("date", leave.date);
+    if (image) {
+      formData.append("image", image);
     }
+
+    console.log(formData);
+    const res = await fetch(`${process.env.REACT_APP_API_SERVER}/leaves`, {
+      method: "POST",
+      headers: {
+        // "Content-Type": "application/json",
+        "x-auth-token": localStorage.getItem("token"),
+      },
+      body: formData,
+    });
+    const data = await res.json();
+    if (res.ok) return data;
+    if (!res.ok) throw new Error(data.message);
+  } catch (e) {
+    throw e.message;
+  }
 };
 
-export const updateLeave = async (id, value) => {
-    try {
-        const res = await fetch(
-            `${process.env.REACT_APP_API_SERVER}/leaves/${id}`,
-            {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-auth-token": localStorage.getItem("token"),
-                },
-                body: JSON.stringify(value),
-            }
-        );
-        const data = await res.json();
-        if (res.ok) return data;
-        if (!res.ok) throw new Error(data.message);
-    } catch (e) {
-        throw e.message;
-    }
+export const updateLeave = async (pending, id) => {
+  try {
+    const res = await fetch(
+      `${process.env.REACT_APP_API_SERVER}/leaves/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": localStorage.getItem("token"),
+        },
+        body: JSON.stringify({ pending }),
+      }
+    );
+    const data = await res.json();
+    if (res.ok) return data;
+    if (!res.ok) throw new Error(data.message);
+  } catch (e) {
+    throw e.message;
+  }
 };
--- client/src/pages/GetApplication.js
+++ client/src/pages/GetApplication.js
@@ -24,45 +24,47 @@ function GetApplication({
   };
   return (
     <>
-      <tbody className=" overflow-y-auto">
-        <tr class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-slate-100">
-          <th
-            scope="row"
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-          >
-            {username}
-          </th>
-          <td class="px-6 py-4">{name}</td>
-          <td class="px-6 py-4">{typeOfLeave}</td>
-          <td class="px-6 py-4 space-x-5">
-            <button
-              value={1}
-              onClick={() => onSubmitHandler(1)}
-              name="pending"
-              class="font-medium text-green-500 hover:underline"
+      {pending == 0 ? (
+        <tbody className=" overflow-y-auto">
+          <tr class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-slate-100">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
-              Approve
-            </button>
-            <button
-              name="pending"
-              value={2}
-              onClick={() => onSubmitHandler(2)}
-              class="font-medium text-red-500 hover:underline"
-            >
-              Reject
-            </button>
-          </td>
-          <td class="px-6 py-4 space-x-5">
-            <a
-              href="#"
-              class="font-medium text-blue hover:underline flex items-center"
-            >
-              View
-              <GrFormView className="text-2xl" />
-            </a>
-          </td>
-        </tr>
-      </tbody>
+              {username}
+            </th>
+            <td class="px-6 py-4">{name}</td>
+            <td class="px-6 py-4">{typeOfLeave}</td>
+            <td class="px-6 py-4 space-x-5">
+              <button
+                value={1}
+                onClick={() => onSubmitHandler(1)}
+                name="pending"
+                class="font-medium text-green-500 hover:underline"
+              >
+                Approve
+              </button>
+              <button
+                name="pending"
+                value={2}
+                onClick={() => onSubmitHandler(2)}
+                class="font-medium text-red-500 hover:underline"
+              >
+                Reject
+              </button>
+            </td>
+            <td class="px-6 py-4 space-x-5">
+              <a
+                href="#"
+                class="font-medium text-blue hover:underline flex items-center"
+              >
+                View
+                <GrFormView className="text-2xl" />
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      ) : null}
     </>
   );
 }
