6a110f9efa5ea506569b3c553c1692b87dcd44f0
Caaron <arn_0000@hotmail.com>
2023-02-14 15:04:54 +0800

    2.6commit

--- client/src/pages/ReviewById.js
+++ client/src/pages/ReviewById.js
@@ -1,74 +1,329 @@
+import { useParams } from "react-router-dom";
+import { getUsersbyId } from "../api/users";
+import { useQuery } from "react-query";
 export const ReviewById = () => {
+  const { id } = useParams();
+  const { data, isLoading } = useQuery(["user", id], () => getUsersbyId(id));
+  console.log(data);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
       <div>
         <div className="flex justify-between mx-16 mt-5">
           <h1 className="text-2xl text-blue font-bold">Performance Review</h1>
+          <div className="flex space-x-8 text-lg">
+            <div className="flex space-x-2">
+              <h1 className="font-bold">Username: </h1>
+              <h1>{data.username}</h1>
+            </div>
+            <div className="flex space-x-2">
+              <h1 className="font-bold">Name: </h1>
+              <h1>{data.name}</h1>
+            </div>
+          </div>
         </div>
-        <div class="relative overflow-x-auto mx-16 max-h-[22rem]">
-          <table class="w-full text-sm text-left text-gray-500">
-            <thead class="text-xs text-gray-700 uppercase bg-sky-100 sticky top-0">
-              <tr>
-                <th scope="col" class="px-6 py-3">
-                  Performance
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Poor
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Good
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Excellent
-                </th>
-              </tr>
-            </thead>
-            <tbody className=" overflow-y-auto">
-              <tr class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-slate-100">
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Productivity
-                </th>
-                <td>
-                  <div class="flex items-center px-6">
-                    <input
-                      id="default-radio-1"
-                      type="radio"
-                      value=""
-                      name="default-radio"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                </td>
-                <td>
-                  <div class="flex items-center">
-                    <input
-                      checked
-                      id="default-radio-2"
-                      type="radio"
-                      value=""
-                      name="default-radio"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                </td>
-                <td>
-                  <div class="flex items-center">
-                    <input
-                      checked
-                      id="default-radio-3"
-                      type="radio"
-                      value=""
-                      name="default-radio"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="flex justify-center mt-28">
+          <div class="relative mx-16 w-[60rem]">
+            <table class="w-full text-sm text-left text-gray-500">
+              <thead class="text-xs text-gray-700 uppercase sticky top-0 border-b-2">
+                <tr>
+                  <th scope="col" class="px-6 py-3">
+                    Performance
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Poor
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Good
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Excellent
+                  </th>
+                </tr>
+              </thead>
+              <tbody className=" overflow-y-auto">
+                <tr class="bg-white">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Productivity
+                  </th>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        id="one-1"
+                        type="radio"
+                        value=""
+                        name="one"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="one-2"
+                        type="radio"
+                        value=""
+                        name="one"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="one-3"
+                        type="radio"
+                        value=""
+                        name="one"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr class="bg-slate-300">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Play Above The Line
+                  </th>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        id="two-1"
+                        type="radio"
+                        value=""
+                        name="two"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="two-2"
+                        type="radio"
+                        value=""
+                        name="two"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="two-3"
+                        type="radio"
+                        value=""
+                        name="two"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr class="bg-white">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Work Consistency
+                  </th>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        id="three-1"
+                        type="radio"
+                        value=""
+                        name="three"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="three-2"
+                        type="radio"
+                        value=""
+                        name="three"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="three-3"
+                        type="radio"
+                        value=""
+                        name="three"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr class="bg-slate-300">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Communication
+                  </th>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        id="four-1"
+                        type="radio"
+                        value=""
+                        name="four"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="four-2"
+                        type="radio"
+                        value=""
+                        name="four"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="four-3"
+                        type="radio"
+                        value=""
+                        name="four"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr class="bg-white">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Attendance
+                  </th>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        id="five-1"
+                        type="radio"
+                        value=""
+                        name="five"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="five-2"
+                        type="radio"
+                        value=""
+                        name="five"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <div class="flex items-center px-6">
+                      <input
+                        checked
+                        id="five-3"
+                        type="radio"
+                        value=""
+                        name="five"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="flex justify-center mx-16 space-x-2 mt-8">
+          <div
+            class="flex justify-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-blue-400"
+            role="alert"
+          >
+            <svg
+              aria-hidden="true"
+              class="flex-shrink-0 inline w-5 h-5 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>Poor = 1 Points</div>
+          </div>
+          <div
+            class="flex justify-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-sky-50 dark:bg-gray-800 dark:text-blue-400"
+            role="alert"
+          >
+            <svg
+              aria-hidden="true"
+              class="flex-shrink-0 inline w-5 h-5 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>Good = 2 Points</div>
+          </div>
+          <div
+            class="flex justify-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-blue-400"
+            role="alert"
+          >
+            <svg
+              aria-hidden="true"
+              class="flex-shrink-0 inline w-5 h-5 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>Excellent = 3 Points</div>
+          </div>
         </div>
       </div>
     </>
--- server/api/users.js
+++ server/api/users.js
@@ -8,143 +8,143 @@ require("dotenv").config();
 
 // super admin registers/create new user
 router.post("/register", auth, async (req, res) => {
-    try {
-        const { name, username, password, isAdmin } = req.body;
-        let userFound = await User.findOne({
-            username,
-        });
-        if (userFound) {
-            return res.status(400).json({
-                message: "User already exists",
-            });
-        }
-
-        const user = new User(req.body);
-        let salt = bcrypt.genSaltSync(10);
-        let hash = bcrypt.hashSync(password, salt);
-        user.password = hash;
-        user.save();
-        return res.json({
-            user,
-            message: "Registered successfully",
-        });
-    } catch (e) {
-        return res.status(400).json({
-            e,
-            message: "Invalid Credentials",
-        });
+  try {
+    const { name, username, password, isAdmin } = req.body;
+    let userFound = await User.findOne({
+      username,
+    });
+    if (userFound) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
+
+    const user = new User(req.body);
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+    user.password = hash;
+    user.save();
+    return res.json({
+      user,
+      message: "Registered successfully",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      e,
+      message: "Invalid Credentials",
+    });
+  }
 });
 
 // all users can login
 router.post("/login", async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        let userFound = await User.findOne({
-            username,
-        });
+  try {
+    const { username, password } = req.body;
+    let userFound = await User.findOne({
+      username,
+    });
 
-        if (!userFound) {
-            return res.status(400).json({
-                message: "Username doesn't exist",
-            });
-        }
-
-        let isMatch = bcrypt.compareSync(password, userFound.password);
-        if (!isMatch) {
-            return res.status(400).json({
-                message: "Invalid Credentials",
-            });
-        }
-
-        jwt.sign(
-            {
-                data: userFound,
-            },
-            process.env.SECRET_KEY,
-            {
-                expiresIn: "1h",
-            },
-            (err, token) => {
-                if (err)
-                    return res.status(400).json({
-                        err,
-                        message: "Cannot generate token",
-                    });
-                return res.json(token);
-            }
-        );
-    } catch (e) {
-        return res.status(400).json({
-            e,
-            message: "Invalid Credentials",
-        });
+    if (!userFound) {
+      return res.status(400).json({
+        message: "Username doesn't exist",
+      });
     }
+
+    let isMatch = bcrypt.compareSync(password, userFound.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    jwt.sign(
+      {
+        data: userFound,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1h",
+      },
+      (err, token) => {
+        if (err)
+          return res.status(400).json({
+            err,
+            message: "Cannot generate token",
+          });
+        return res.json(token);
+      }
+    );
+  } catch (e) {
+    return res.status(400).json({
+      e,
+      message: "Invalid Credentials",
+    });
+  }
 });
 
 // fetches all users
 router.get("/", auth, async (req, res) => {
-    try {
-        let users = await User.find({});
-        if (!users.length) return res.json({ message: "No users found" });
-        return res.json(users);
-    } catch (e) {
-        return res.json({
-            e,
-            message: "Failed to get users",
-        });
-    }
+  try {
+    let users = await User.find({});
+    if (!users.length) return res.json({ message: "No users found" });
+    return res.json(users);
+  } catch (e) {
+    return res.json({
+      e,
+      message: "Failed to get users",
+    });
+  }
 });
 
 // admin can get/request a single user information
-router.get("/:id", auth, async (req, res) => {
-    try {
-        let user = await User.findById(req.params.id);
-        if (!user)
-            return res.status(400).json({
-                message: "No user found",
-            });
-        return res.json(user);
-    } catch (e) {
-        return res.status(400).json({
-            e,
-            message: "Cannot get user",
-        });
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user)
+      return res.status(400).json({
+        message: "No user found",
+      });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json({
+      e,
+      message: "Cannot get user",
+    });
+  }
 });
 
 // super admin can delete a user
 router.delete("/:id", auth, async (req, res) => {
-    try {
-        let user = await User.findByIdAndDelete(req.params.id);
-        return res.json({
-            user,
-            message: "User successfully deleted",
-        });
-    } catch (e) {
-        return res.status(400).json({
-            e,
-            message: "Failed to delete user",
-        });
-    }
+  try {
+    let user = await User.findByIdAndDelete(req.params.id);
+    return res.json({
+      user,
+      message: "User successfully deleted",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      e,
+      message: "Failed to delete user",
+    });
+  }
 });
 
 // super admin can edit a user
 router.put("/:id", auth, async (req, res) => {
-    try {
-        let user = await User.findById(req.params.id);
-        if (!user) return res.json({ message: "User does not exist" });
-        user = await User.findByIdAndUpdate(req.params.id, req.body);
-        return res.json({
-            user,
-            message: "Successfully updated user",
-        });
-    } catch (e) {
-        return res.status(400).json({
-            e,
-            message: "Cannot edit user",
-        });
-    }
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user) return res.json({ message: "User does not exist" });
+    user = await User.findByIdAndUpdate(req.params.id, req.body);
+    return res.json({
+      user,
+      message: "Successfully updated user",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      e,
+      message: "Cannot edit user",
+    });
+  }
 });
 
 module.exports = router;
