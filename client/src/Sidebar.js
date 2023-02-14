9bba516d90825940c40361e1d79ee0745ebef032
Caaron <arn_0000@hotmail.com>
2023-02-14 11:05:51 +0800

    2.5commit

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,5 +1,6 @@
 import { RxDashboard } from "react-icons/rx";
 import { FiUser } from "react-icons/fi";
+import { FiUsers } from "react-icons/fi";
 import { BiLogOut } from "react-icons/bi";
 import { BiCalendarCheck } from "react-icons/bi";
 import { RiHandCoinLine } from "react-icons/ri";
@@ -8,33 +9,36 @@ import { useNavigate } from "react-router-dom";
 import { logout } from "./api/users";
 
 export const Sidebar = () => {
-    let navigate = useNavigate();
-    return (
-        <div className="grid grid-cols-1 content-between h-screen">
-            <div className="flex flex-col mx-auto text-4xl text-white space-y-10 mt-20">
-                <NavLink to="dashboard">
-                    <RxDashboard />
-                </NavLink>
-                <NavLink to="user">
-                    <FiUser />
-                </NavLink>
-                <NavLink to="application">
-                    <BiCalendarCheck />
-                </NavLink>
-                <RiHandCoinLine />
-            </div>
-            <div className="flex flex-col mx-auto text-4xl text-white space-y-10 mb-5">
-                <button
-                    onClick={() => {
-                        localStorage.removeItem("username");
-                        logout();
-                        navigate("/");
-                        window.location.reload();
-                    }}
-                >
-                    <BiLogOut />
-                </button>
-            </div>
-        </div>
-    );
+  let navigate = useNavigate();
+  return (
+    <div className="grid grid-cols-1 content-between h-screen">
+      <div className="flex flex-col mx-auto text-4xl text-white space-y-10 mt-20">
+        <NavLink to="dashboard">
+          <RxDashboard />
+        </NavLink>
+        <NavLink to="user">
+          <FiUsers />
+        </NavLink>
+        <NavLink to="application">
+          <BiCalendarCheck />
+        </NavLink>
+        <NavLink to="profile">
+          <FiUser />
+        </NavLink>
+        <RiHandCoinLine />
+      </div>
+      <div className="flex flex-col mx-auto text-4xl text-white space-y-10 mb-5">
+        <button
+          onClick={() => {
+            localStorage.removeItem("username");
+            logout();
+            navigate("/");
+            window.location.reload();
+          }}
+        >
+          <BiLogOut />
+        </button>
+      </div>
+    </div>
+  );
 };
